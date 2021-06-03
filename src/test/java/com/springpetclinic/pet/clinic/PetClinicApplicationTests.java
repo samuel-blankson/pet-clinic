579283e12da34d559444623988bf056ae1ba516d
samuel-blankson <72501108+samuel-blankson@users.noreply.github.com>
2021-06-03 22:13:48 +0000

    initial project commit

---
+++ src/test/java/com/springpetclinic/pet/clinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springpetclinic.pet.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
