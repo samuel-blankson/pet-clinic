fdd7867c4e28c88d9ddcc4bcbce107fe50cd3142
samuel-blankson <72501108+samuel-blankson@users.noreply.github.com>
2021-06-09 14:26:45 +0000

    service interface implemented

---
+++ pet-clinic-data/src/main/java/services/VetService.java
@@ -0,0 +1,15 @@
+package services;
+
+import model.Pet;
+import model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
